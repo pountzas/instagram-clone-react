16314c26a1ebe6ab2ff7a4486487131641ed87da
pountzas <nikos@pountzas.gr>
2021-11-26 21:30:42 +0200

    add login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,23 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
 export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const isInvalid = password === '' || email === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
   return (
     <p>login page</p>
   );
